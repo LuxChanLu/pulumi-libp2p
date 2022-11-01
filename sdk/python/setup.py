c2925ba01a2324e55455d7e70f5531f562ab3849
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2022-11-01 12:54:01 +0100

    fix deps

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -54,7 +54,6 @@ setup(name='pulumi_libp2p',
       install_requires=[
           'parver>=0.2.1',
           'pulumi>=3.0.0,<4.0.0',
-          'pulumi-libp2p>=1.0.1',
           'semver>=2.8.1'
       ],
       zip_safe=False)
